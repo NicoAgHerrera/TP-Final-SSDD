e46c656441cd028e88fd877ffa1b4a5497776182
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2025-11-24 19:31:32 -0300

    Borro providers (quedo viejo)

--- convertapp/app/layout.tsx
+++ convertapp/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { ConversionProvider } from "../contexts/ConversionContext";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Providers } from "./providers";
 
 export const metadata = {
   title: "ConvertApp",
@@ -21,12 +20,10 @@ export default function RootLayout({
         <ConversionProvider>
           {/* HEADER */}
           <Header />
-          <Providers>
             {/* CONTENIDO */}
             <main className="flex-1 max-w-3xl mx-auto w-full py-0 px-4">
               {children}
             </main>
-          </Providers>
           {/* FOOTER pegado abajo siempre */}
           <Footer />
         </ConversionProvider>
